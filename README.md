d6d8fdbc3639e44239f9759e0c26626302457be4
Alex Johnson <alex.johnson293@gmail.com>
2023-02-26 12:34:45 -0800

    template cards & photo carousel

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ TODO
 * host it
 
 
+## Uses
+holderjs for placeholder images (included in bootstrap already...oops!)
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,57 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
 function Projects(){
   return (
     <React.Fragment>
-    <h1>Look at my github!</h1>
-    <p> this is the projects page</p>
+      <h1>Look at my github!</h1>
+      <Col className="d-flex">
+          <Card className="flex-fill" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px181" />
+          <Card.Body>
+            <Card.Title>Project 1</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">github link</Button>
+          </Card.Body>
+        </Card>
+        <Card className="flex-fill" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Project 2</Card.Title>
+            <Card.Text>
+              Yadda yadda should be in the same row as project #1 hope it works
+            </Card.Text>
+            <Button variant="primary">github link</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="flex-fill" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Project 3</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">github link</Button>
+          </Card.Body>
+        </Card>
+        <Card className="flex-fill" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Project 4</Card.Title>
+            <Card.Text>
+              Yadda yadda should be in the same row as project #1 hope it works
+            </Card.Text>
+            <Button variant="primary">github link</Button>
+          </Card.Body>
+        </Card>
+        </Col>
     </React.Fragment>
   );
 }
