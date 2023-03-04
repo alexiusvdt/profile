f02879f5aa8c321211a74f3ccfe7492bd1704edb
Alex Johnson <alex.johnson293@gmail.com>
2023-03-04 09:57:54 -0800

    hard coding projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,22 +11,29 @@ function Projects(){
           <Card className="flex-fill" style={{ width: '18rem' }}>
           <Card.Img variant="top" src="holder.js/100px181" />
           <Card.Body>
-            <Card.Title>Project 1</Card.Title>
+            <Card.Title>Help Queue</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              A help ticket queue using React that stores tickets in a firebase database.
             </Card.Text>
-            <Button variant="primary">github link</Button>
+            <Button variant="primary" onClick={(e) => {
+              e.preventDefault();
+              window.location.href='https://github.com/alexiusvdt/help-queue-hooks'
+              }}>github link
+            </Button>
           </Card.Body>
         </Card>
         <Card className="flex-fill" style={{ width: '18rem' }}>
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
-            <Card.Title>Project 2</Card.Title>
+            <Card.Title>To Do List 2</Card.Title>
             <Card.Text>
-              Yadda yadda should be in the same row as project #1 hope it works
+              An ASP.NET to do list application that stores entries in a SQL database.
             </Card.Text>
-            <Button variant="primary">github link</Button>
+            <Button variant="primary" onClick={(e) => {
+                e.preventDefault();
+                window.location.href='https://github.com/alexiusvdt/ToDoList-Styled'
+                }}>github link
+              </Button>
           </Card.Body>
         </Card>
 
