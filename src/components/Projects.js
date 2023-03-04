53dc02a1d6a5a72e4105c94c3e71c77fca1022b7
Alex Johnson <alex.johnson293@gmail.com>
2023-03-04 10:33:44 -0800

    changing project ul to cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,19 +1,45 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
+import { Card } from "react-bootstrap";
 import { projects } from '../config';
 import styled from "styled-components";
 
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+
+  li {
+    padding: 5px;
+    &: last-of-type {
+      // margin-bottom: 10px;
+    }
+
+    a {
+      padding: 5px;
+      &: hover,
+      &: focus {
+        // transform: translate Y(5px);
+      }
+      
+    }
+  }
+`;
 
 function Projects(){
   return (
     <>
     <h1>Look at my projects!</h1>
+    {/* <StyledList> */}
       {projects.map(( {name, url, blurb}, i)  => {   
-        return (<li key={i}>
+        <Card>
+        <li key={i}>
           <a href={url} target="_blank" rel="noreferrer">{name}</a><p>{blurb}</p></li>) 
       })}
+      </Card>
+      // </StyledList>
     </>
   );
 }
