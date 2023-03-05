a0039ce589adef0831ed26bece09a982515a5d6f
Alex Johnson <alex.johnson293@gmail.com>
2023-03-05 11:46:33 -0800

    WIP: sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,45 @@
 import React, {useState, useEffect} from "react";
 import styled from "styled-components";
 
-const headerStyle = {
-  position: "sticky",
-  top: 10,
-  backgroundColor: "#BFDBFE",
-  height: "6rem",
-  transitionProperty: "all",
-  transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
-  transitionDuration: "500ms",
-}
+// const headerStyle = {
+//   position: 'sticky',
+//   top: 10,
+//   backgroundColor: '#BFDBFE',
+//   height: "6rem",
+//   transitionProperty: "all",
+//   transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
+//   transitionDuration: "500ms",
+// }
 
 function Header() {
-  const scrollDirection = useScrollDirection();
+//   const scrollDirection = useScrollDirection();
   
-  function useScrollDirection() {
-    const [scrollDirection, setScrollDirection] = useState(null);
+//   function useScrollDirection() {
+//     const [scrollDirection, setScrollDirection] = useState(null);
   
-    useEffect(() => {
-      let lastScrollY = window.pageYOffset;
+//     useEffect(() => {
+//       let lastScrollY = window.pageYOffset;
   
-      const updateScrollDirection = () => {
-        const scrollY = window.pageYOffset;
-        const direction = scrollY > lastScrollY ? "down" : "up";
-        if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
-          setScrollDirection(direction);
-        }
-        lastScrollY = scrollY > 0 ? scrollY : 0;
-      };
-      window.addEventListener("scroll", updateScrollDirection); // add event listener
-      return () => {
-        window.removeEventListener("scroll", updateScrollDirection); // clean up
-      }
-    }, [scrollDirection]);
+//       const updateScrollDirection = () => {
+//         const scrollY = window.pageYOffset;
+//         const direction = scrollY > lastScrollY ? "down" : "up";
+//         if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
+//           setScrollDirection(direction);
+//         }
+//         lastScrollY = scrollY > 0 ? scrollY : 0;
+//       };
+//       window.addEventListener("scroll", updateScrollDirection); // add event listener
+//       return () => {
+//         window.removeEventListener("scroll", updateScrollDirection); // clean up
+//       }
+//     }, [scrollDirection]);
   
-    return scrollDirection;
-  };
+//     return scrollDirection;
+//   };
 
   return (
     <>
-      <div className={`headerStyle ${scrollDirection === "down" ? "hide" : "show" }`}>
+      <div>
         <H1>Hi! I'm Alex.</H1>
       </div>
     </>
