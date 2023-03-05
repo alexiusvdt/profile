1f5a01c9fd549c8ab4a98d6e0effc038f47eb78c
Alex Johnson <alex.johnson293@gmail.com>
2023-03-04 16:18:13 -0800

    add border for visual ease of use, style later

--- src/components/PageControl.js
+++ src/components/PageControl.js
@@ -1,30 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Socials from './Socials';
+import Projects from './Projects';
+import ContactForm from './ContactMe';
 
+import styled from "styled-components";
+import Sidebar from './Sidebar';
 
-function PageControl() {
-  const [isHome, setIsHome] = useState(true);
+const Container = styled.div`
+  display: flex;
 
-// set up a useEffect for branching conditionals.
-// if loading, return loading image
-// if not loading & on home feed first image
+`;
+//whatever we pass in to pane will replace the default weight here
+const Pane = styled.div`
+  flex: ${props => props.weight};
+  border: solid 3px black;
+`;
 
-  let currentlyVisibleState = "placeholder state";
-  let buttonText = "bottom text";
-
-  // if(welcomeVisibleOnPage){
-  //   currentlyVisibleState = <WelcomeSplash />
-  //   buttonText = "Come take a look!";
-  // } else if(!welcomeVisibleOnPage){
-  //   currentlyVisibleState = <MainPage />
-  //   buttonText = "this is a button";
-  // }
-  if(isHome)
-    return (
-      <React.Fragment>      
-        {/* <Socials /> */}
-      </React.Fragment>
-    );
+const PageControl = ({
+  leftWeight = 0.75,
+  rightWeight = 4,
+}) => {
+  return (
+    <Container>
+      <Pane weight={leftWeight}>
+        <Sidebar />
+      </Pane>
+      <Pane weight={rightWeight}>
+        <Projects />
+      </Pane>
+    </Container>
+  );
 }
 
 export default PageControl;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Sidebar(){
-  render(
+  return(
     <p>sidebar!</p>
   );
 }
