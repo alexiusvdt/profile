cbb2b675457f4d5fa2a0dbc283899f27e436d335
Alex Johnson <alex.johnson293@gmail.com>
2023-03-07 09:08:03 -0800

    making progress

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,21 @@
 import React from "react";
-// only need the function...
+
+// sidebar is selecting the content & switching it to pagecontrol
+// pagecontrol is rendering left & right bar
+// pass the prop (ie content to render) up to pagecontrol
+// in pagecontrol, render whatever prop is passed to it
+// dont try to copy props into state because that causes errors 
 
 
 function Sidebar(){
+  // const handleSwitch = () => undefinedFunc("hi")
+
   return(
     <>
     <p>sidebar!</p>
-    <button >Projects</button>
+    <button 
+      // onClick={handleSwitch}
+      >switch</button>
     <button >ContactMe</button>
     </>
   );
