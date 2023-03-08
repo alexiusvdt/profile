560da397cf14abc860e43a9e9c1e066a88cd735a
Alex Johnson <alex.johnson293@gmail.com>
2023-03-07 17:30:49 -0800

    WIP: main styling, working on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,71 +1,29 @@
 import React, {useState, useEffect} from "react";
 import styled from "styled-components";
+import { motion as m } from 'framer-motion';
 
-const headerStyle = {
-  position: 'sticky',
-  top: 10,
-  backgroundColor: '#BFDBFE',
-  height: "6rem",
-  transitionProperty: "all",
-  transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
-  transitionDuration: "500ms",
-}
 
 function Header() {
-  // const scrollDirection = useScrollDirection();
-  
-  // function useScrollDirection() {
-  //   const [scrollDirection, setScrollDirection] = useState(null);
-  
-  //   useEffect(() => {
-  //     let lastScrollY = window.pageYOffset;
-  
-  //     const updateScrollDirection = () => {
-  //       const scrollY = window.pageYOffset;
-  //       const direction = scrollY > lastScrollY ? "down" : "up";
-  //       if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
-  //         setScrollDirection(direction);
-  //       }
-  //       lastScrollY = scrollY > 0 ? scrollY : 0;
-  //     };
-  //     window.addEventListener("scroll", updateScrollDirection); // add event listener
-  //     return () => {
-  //       window.removeEventListener("scroll", updateScrollDirection); // clean up
-  //     }
-  //   }, [scrollDirection]);
-  
-  //   return scrollDirection;
-  // };
 
   return (
     <>
-    {/* styling isn't applying, why? tried nesting & alternate methods...maybe try inline styling next? */}
-      <div>
-      {/* <div style={`headerStyle ${scrollDirection === "down" ? "hide" : "show"}`}> */}
+      <m.div>
         <H1>Hi! I'm Alex.</H1>
-      </div>
+      </m.div>
     </>
   );
 }
 
 export default Header;
 
-// STYLING
-// this can probably get deleted
-
-// const P = styled.p`
-//   color: #fff;
-// `
-
+// 
 const H1 = styled.h1 `
-  margin-left: 250px;  
+  margin-left: 280px;  
   margin-top: 0px;
-  // margin-bottom: 10px;
   font-family: sans-serif;
   font-size: 6rem;
   background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
   background-clip: border-box ;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  /* border: 1px solid green; */
 `
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,8 @@ function Main(){
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     transition={{ duration: 0.75, ease: "easeOut" }}
-    className="text-gray-900 absolute top-0 left-0 w-full h-full bg-teal-100 lg:px-48 px-16">
+    className='text-gray-900 absolute top-0 left-0 w-full h-full bg-orange-100 lg:px-48 px-16'
+    >
       <div className="my-96 p-1">
         <h1 className="text-6x1 text-center lg:text-right lg:text-9x1">
           Hello there!
