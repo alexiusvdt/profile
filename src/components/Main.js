88e3f6c2406c684ffc055de3bf05f884ecf3a1be
Alex Johnson <alex.johnson293@gmail.com>
2023-03-07 17:02:06 -0800

    WIP: coloring/style not applying for transition element

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,10 +6,10 @@ function Main(){
     <m.div 
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
-    transition={{ duration: 0.75 }}
-    >
+    transition={{ duration: 0.75, ease: "easeOut" }}
+    className="text-gray-900 absolute top-0 left-0 w-full h-full bg-teal-100 lg:px-48 px-16">
       <div className="my-96 p-1">
-        <h1 className="text-6x1 text-center lg:text-right lg: text-9x1">
+        <h1 className="text-6x1 text-center lg:text-right lg:text-9x1">
           Hello there!
         </h1>
       </div>
