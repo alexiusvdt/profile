09b9f71739a769b0e08620a90c29a5ca94c0fa62
Alex Johnson <alex.johnson293@gmail.com>
2023-03-08 19:52:47 -0800

    roll back animation change for stable restore point

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,38 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { motion as m } from 'framer-motion';
-import { SCREAMCAT } from '../img/cat-transp';
 
 // this m.div still needs work!
 
 function Main(){
-  const [animate, setAnimate] = useState(true)
-
-  useEffect(() => {
-    setAnimate(false)
-  }, []);
-
   return(
-    <div>
-      {animate && (
-      <m.div 
-        initial={{ y: '100vh' }}
-        animate={{ y: '50vh' }}
-        transition={{ type: 'spring', stiffness: 100, damping: 20, duration: 1, ease: "easeOut" }}
-        >   
-          <img
-            src={ SCREAMCAT }
-            alt="A lil' cat"
-            style={{justifyContent: 'center'}}
-            className={`image-animation ${animate ? "active" : ""}`}
-          /> 
-      </m.div>
-      )}
-    </div>
+    <m.div 
+      initial={{opacity: 0}}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, ease: "easeOut" }}
+      >
+      <div>
+        <h1 style={{textAlign: 'center', justifyContent: 'center',  color: '#fff'}}>
+          Fgsfds!
+        </h1>
+      </div>
+    </m.div>
   );
 }
 
 export default Main;
-
-//<h1 style={{textAlign: 'center', justifyContent: 'center',  color: '#fff'}}>
-//           Fgsfds!
-//         </h1> 
