a09ce612ba150e623ec3bc92be6b9e0e87753e88
Alex Johnson <alex.johnson293@gmail.com>
2023-03-08 19:41:13 -0800

    cleanup images, add tmep

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion as m } from 'framer-motion';
+import { SCREAMCAT } from '../img/cat-transp';
 
 // this m.div still needs work!
 
 function Main(){
+  const [animate, setAnimate] = useState(true)
+
+  useEffect(() => {
+    setAnimate(false)
+  }, []);
+
   return(
-    <m.div 
-      initial={{ y: '100vh' }}
-      animate={{ y: '50vh' }}
-      transition={{ type: 'spring', stiffness: 100, damping: 20, duration: 1, ease: "easeOut" }}
-      >
-      <div>
-        {/* <img></img> */}
-        <h1 style={{textAlign: 'center', justifyContent: 'center',  color: '#fff'}}>
-          Fgsfds!
-        </h1>
-      </div>
-    </m.div>
+    <div>
+      {animate && (
+      <m.div 
+        initial={{ y: '100vh' }}
+        animate={{ y: '50vh' }}
+        transition={{ type: 'spring', stiffness: 100, damping: 20, duration: 1, ease: "easeOut" }}
+        >   
+          <img
+            src={ SCREAMCAT }
+            alt="A lil' cat"
+            style={{justifyContent: 'center'}}
+            className={`image-animation ${animate ? "active" : ""}`}
+          /> 
+      </m.div>
+      )}
+    </div>
   );
 }
 
 export default Main;
+
+//<h1 style={{textAlign: 'center', justifyContent: 'center',  color: '#fff'}}>
+//           Fgsfds!
+//         </h1> 
