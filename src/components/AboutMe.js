f3d34596a73c0cfe78ab7647fa95dc1f5d4ef68e
Alex Johnson <alex.johnson293@gmail.com>
2023-03-08 18:36:00 -0800

    fix display of aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,18 +11,20 @@ function AboutMe(){
       animate={{opacity: 1}}
       transition={{ duration: 0.5}}
       >
-        <div style={{ display:"flex", alignItems: "center", flexDirection: "row"}}>
-          <P>Hi, I'm Alex, a junior developer looking to make their mark on the industry. 
-            I specialize in creating user-friendly websites and mobile apps with an eye to accessability 
-            as well as functionality and stability. I'm passionate about creating unique and innovative 
-            applications that help businesses achieve their goals. In my free time, I enjoy exploring 
-            new technologies, riding my bicycle (currently training for <a href="https://ridetherimoregon.com/" target="_blank" rel="noreferrer">Ride The Rim Oregon</a>) 
-            and playing tabletop role playing games with my friends. My ultimate goal is to create designs that 
-            not only look great but also solve real-world problems for my clients!</P>
-        </div>
-        <div>
-          <img src={Headshot} width={450} height={350} alt="My headshot"></img>
-        </div>
+       <div style ={{display: 'flex', alignItems: 'center', flexDirection: 'row'}}>
+          <div style={{ display:"flex", alignItems: "center", flexDirection: "row"}}>
+            <P>Hi, I'm Alex, a junior developer looking to make their mark on the industry. 
+              I specialize in creating user-friendly websites and mobile apps with an eye to accessability 
+              as well as functionality and stability. I'm passionate about creating unique and innovative 
+              applications that help businesses achieve their goals. In my free time, I enjoy exploring 
+              new technologies, riding my bicycle (currently training for <a href="https://ridetherimoregon.com/" target="_blank" rel="noreferrer">Ride The Rim Oregon</a>) 
+              and playing tabletop role playing games with my friends. My ultimate goal is to create designs that 
+              not only look great but also solve real-world problems for my clients!</P>
+          </div>
+          <div>
+            <img src={Headshot} width={450} height={350} alt="My headshot"></img>
+          </div>
+        </div> 
     </m.div>
    
     </>
