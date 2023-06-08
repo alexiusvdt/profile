e29f4491b063f5816f03f7b143af622d88b5d7f5
alexiusvdt <alex.johnson293@gmail.com>
2023-06-08 14:36:07 -0700

    WIP: adding cubes. No errors, should be showing in scene but not appearing in DOM

---
+++ src/components/Scene.js
@@ -0,0 +1,16 @@
+/*eslint-disable*/
+import React, { ReactDOM }from 'react';
+import { Canvas } from '@react-three/fiber';
+import Cubes from './Cubes';
+
+const Scene = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <Cubes />
+    </Canvas>
+  );
+};
+
+export default Scene;
