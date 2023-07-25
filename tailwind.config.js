71ef2a5529e9304dc2e3ce7624ac51cb5fb44feb
alexiusvdt <alex.johnson293@gmail.com>
2023-07-25 11:59:21 -0700

    WIP: wrestling with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 // this is the default theme from https://tailwindcss.com/docs/theme
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   // tailwind must know about every single file in the project that contains any class names
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    "./src/**/*.js",
+    "./src/components/**.js",
+    "./public/index.html"
+  ],
   presets: [],
   darkMode: 'class', // or 'media' to base off users' OS
   // theme holds customizations for screens, colors, and spacing as well as core plugins
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif']
+    },
   },
   variants : {
     extend:{}
   },
   plugins: [],
+  }
 }
