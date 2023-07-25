6822cfceaa68b5a6fd7f1375a98214c923088b35
alexiusvdt <alex.johnson293@gmail.com>
2023-07-25 12:12:50 -0700

    resolved: tailwind import

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,14 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   // tailwind must know about every single file in the project that contains any class names
-  content: [
-    "./src/**/*.js",
-    "./src/components/**.js",
-    "./public/index.html"
-  ],
+  content: {
+    relative: true,
+    files: [
+      "./public/index.html",
+      "./src/**/*.{js}",
+    "./src/components/**.{js}",
+    ],
+  },
   presets: [],
   darkMode: 'class', // or 'media' to base off users' OS
   // theme holds customizations for screens, colors, and spacing as well as core plugins
