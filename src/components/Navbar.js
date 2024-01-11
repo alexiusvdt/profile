8d7e38c53b6aec5fe64479d72c053b9ea3f73744
alexiusvdt <alex.johnson293@gmail.com>
2024-01-10 21:39:40 -0800

    linting linting linting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,96 +1,99 @@
-import React, { useEffect, useState } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
-import blackcat from "../img/black_cat.png";
-import whitecat from "../img/white_cat.png";
+import React, { useEffect, useState } from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
+/* eslint-ignore next line */
 const Navbar = (props) => {
   const [show, setShow] = useState(false);
-  
+
   const controlNavbar = () => {
-    if (window.scrollY > 20){
+    if (window.scrollY > 20) {
       setShow(true);
     } else {
       setShow(false);
     }
   };
 
+  /* eslint-ignore next line */
   const scrollToTop = () => {
     scroll.scrollToTop();
-    };
+  };
 
-  useEffect(() => {window.addEventListener('scroll', controlNavbar);
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
     return () => {
       window.removeEventListener('scroll', controlNavbar);
     };
   }, []);
 
-    return <div className={`nav ${show && 'nav-static'} id="navbar`}>
+  return (
+    <div className={`nav ${show && 'nav-static'} id="navbar`}>
       <div className="nav-content">
         <div className="flex flex-shrink-0 items-center object-cover">
-        </div>
-        <ul className="nav-items">
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="aboutme"
-              spy={true}
-              smooth='easeInQuart'
-              offset={-70}
-              duration={500}
-            >
-              About Me 
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="skills"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Skills              
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="projects"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              Projects
-            </Link>
-          </li>
+          {/* </div> */}
+          <ul className="nav-items">
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="aboutme"
+                spy={true}
+                smooth='easeInQuart'
+                offset={-70}
+                duration={500}
+              >
+                About Me 
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="skills"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Skills              
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Projects
+              </Link>
+            </li>
 
-          <li className="nav-item">
-            <Link
-              activeClass="active"
-              to="contactme"
-              spy={true}
-              smooth='linear'
-              offset={-70}
-              duration={500}
-              delay={100}
-            >
-              Contact Me
-            </Link>
-          </li>
-          <li className="nav-item">
-            <div className="switch" title="toggle dark/light">
-              <label className="theme-switch" htmlFor="checkbox">
-              <input type="checkbox" id="checkbox" onClick={props.toggleDarkMode} />
-              <div className="slider round"></div>
-              </label>
-            </div>
-          </li>
-        </ul>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="contactme"
+                spy={true}
+                smooth='linear'
+                offset={-70}
+                duration={500}
+                delay={100}
+              >
+                Contact Me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <div className="switch" title="toggle dark/light">
+                <label className="theme-switch" htmlFor="checkbox">
+                <input type="checkbox" id="checkbox" onClick={props.toggleDarkMode} />
+                <div className="slider round"></div>
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      {/* eslint-ignore */}
-  </div>;
+    </div>
+  );
 };
 
 export default Navbar;
